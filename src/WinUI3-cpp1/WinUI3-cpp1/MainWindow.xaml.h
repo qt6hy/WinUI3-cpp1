25ff84fa6cbad529e551d585c85f34a231b3152d
qt6hy <qt6.bluesky@gmail.com>
2022-08-17 17:44:21 +0900

    [update] rename folder

---
+++ src/WinUI3-cpp1/WinUI3-cpp1/MainWindow.xaml.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#pragma push_macro("GetCurrentTime")
+#undef GetCurrentTime
+
+#include "MainWindow.g.h"
+
+#pragma pop_macro("GetCurrentTime")
+
+namespace winrt::WinUI3_cpp1::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void myAppBarButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void mySendMail_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::WinUI3_cpp1::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
